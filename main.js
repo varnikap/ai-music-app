7a3a1be55804a4a96f391890226e5db02601209f
varnikap <78656240+varnikap@users.noreply.github.com>
2022-01-22 16:39:00 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 music1 =""; 
 music2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
@@ -17,9 +21,31 @@ function setup()
     canvas.position(400,250);
     video = createCapture(VIDEO);
 video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded()
+{
+console.log("PoseNet is Initialized");
 }
 
 function draw()
 {
     image( video, 0, 0, 600, 500);
+}
+
+function gotPoses(results)
+{
+if(results>0)
+{
+    leftWristX = results[0].pose.leftWrist.x;
+    leftWristX = results[0].pose.leftWrist.x;
+    console.log("leftWristX = " + leftWristX  + "leftWristY = " + leftWristY);
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    console.log("rightWristX = " + rightWristX  + "rightWristY = " + rightWristY);
+}
 }
